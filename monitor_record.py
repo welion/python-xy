3fee130434c9428a24f4c0caea22f34dd831af9d
welion <353566165@qq.com>
2016-05-01 18:21:53 +0800

    20160501
    add dummy record

--- monitor_record.py
+++ monitor_record.py
@@ -8,33 +8,43 @@ Created on Thu Apr 28 17:31:54 2016
 record = {}
 #业务层：
 ##银行卡交易成功率（TRAN_SUC）
-##交易笔数（TRAN_NUM)
-##交易时长(TRAN_TIME)
+##每分钟交易量（TRAN_TPM）
+##平均交易时长(TRAN_TIME)
 ##交易承兑率(TRAN_ACC)
 ##冲正率(TRAN_REV)
 ##交易类型(TRAN_TYPE)
-tran_rec = {"TRAN_SUC":"",
-            "TRAN_NUM":"",
-            "TRAN_TIME":"",
-            "TRAN_ACC":"",
-            "TRAN_REV":"",
-            "TRAN_TYPE":"",}
+tran_rec = {"TRAN_SUC":"40",
+            "TRAN_TPM":"21043",
+            "TRAN_TIME":"1.2",
+            "TRAN_ACC":"32",
+            "TRAN_REV":"56",
+            "TRAN_TYPE":"ISO-8583",}
 #应用层：
-##进程个数(APP_PRC)、
+##通讯进程个数(APP_CTP)、
+##交易处理进程个数（APP_NOF）
 ##消息队列个数（APP_MQN)
 ##消息队列拥堵状况(APP_MQS)
 ##日志刷新频率(APP_LOG)
-app_rec={"APP_PRC":"",
-         "APP_MQN":"",
-         "APP_MQS":"",
-         "APP_LOG":"",}
+##应用句柄数(APP_HAND)
+app_rec={"APP_CTP":"128",
+         "APP_NOF":"16",
+         "APP_MQN":"24",
+         "APP_MQS":"12",
+         "APP_LOG":"16",
+         "APP_HAND":"3792",}
 #数据库层：
+##数据库CPU使用率(DB_CPU)
+##数据库CACHE命中率(DB_HIR)
+##数据库索引块分裂（DB_IDXCON）
 ##oracle锁表(DB_LOCK)
 ##表空间利用率(DB_TAB)
 ##数据库等待事件(DB_WAIT)
-db_rec={"DB_LOCK":"",
-        "DB_TAB":"",
-        "DB_WAIT":"",}
+db_rec={"DB_CPU":"63",
+        "DB_HIR":"95",
+        "DB_IDXCON":"False",
+        "DB_LOCK":"5",
+        "DB_TAB":"75",
+        "DB_WAIT":"10",}
 #操作系统层：
 ##PING状态（OS_PING)
 ##CPU(OS_CPU)
@@ -42,21 +52,21 @@ db_rec={"DB_LOCK":"",
 ##SWAP(OS_SWAP)
 ##Storage(OS_STOR)
 ##磁盘响应时间(OS_IO)
-os_rec={"OS_OING":"",
-        "OS_CPU":"",
-        "OS_MEM":"",
-        "OS_SWAP":"",
-        "OS_STOR":"",
-        "OS_IO":"",}
+os_rec={"OS_PING":"True",
+        "OS_CPU":"67",
+        "OS_MEM":"64",
+        "OS_SWAP":"0",
+        "OS_STOR":"63",
+        "OS_IO":"0.0010",}
 #网络：
 ##通断性(NET_LINK)
 ##丢包率(NET_LOSS)
 ##连接数(NET_LINKNUM)
 ##带宽使用率(NET_BW)
-net_rec={"NET_LINK":"",
-         "NET_LOSS":"",
-         "NET_LINKNUM":"",
-         "NET_BW":"",}
+net_rec={"NET_LINK":"True",
+         "NET_LOSS":"0",
+         "NET_LINKNUM":"3420",
+         "NET_BW":"14",}
 #环境：
 ##火灾状态(PHY_FIRE)
 ##渗水报警(PHY_WATER)
@@ -64,12 +74,12 @@ net_rec={"NET_LINK":"",
 ##电压(PHY_VOL)
 ##机房温度(PHY_TEM)
 ##机房湿度(PHY_HUM)
-phy_rec={"PHY_FIRE":"",
-         "PHY_WATER":"",
-         "PHY_POW":"",
+phy_rec={"PHY_FIRE":"True",
+         "PHY_WATER":"True",
+         "PHY_POW":"True",
          "PHY_VOL":"",
-         "PHY_TEM":"",
-         "PHY_NUM":"",}
+         "PHY_TEM":"23",
+         "PHY_HUM":"45",}
 
 record.update(tran_rec)
 record.update(app_rec)
@@ -79,5 +89,7 @@ record.update(net_rec)
 record.update(phy_rec)
 
 print record
+
+
 
 
