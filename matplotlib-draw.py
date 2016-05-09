3cf8ac26019c8a9412e39f12ffb70296e8ea959c
welion <353566165@qq.com>
2016-05-09 20:06:07 +0800

    20160509

--- matplotlib-draw.py
+++ matplotlib-draw.py
@@ -7,7 +7,49 @@ Created on Sat May 07 10:38:00 2016
 import matplotlib.pylab as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
- 
+
+
+tran_rec1 = {"TRAN_SUC":"60",
+            "TRAN_TPM":"21.243",
+            "TRAN_TIME":"80",
+            "TRAN_ACC":"54",
+            "TRAN_REV":"42",
+            "TRAN_TYPE":"100",}
+
+app_rec1={"APP_CTP":"128",
+         "APP_NOF":"16",
+         "APP_MQN":"24",
+         "APP_MQS":"12",
+         "APP_LOG":"16",
+         "APP_HAND":"3792",}
+
+db_rec1={"DB_CPU":"63",
+        "DB_HIR":"95",
+        "DB_IDXCON":"0",
+        "DB_LOCK":"5",
+        "DB_TAB":"75",
+        "DB_WAIT":"10",}
+
+os_rec1={"OS_PING":"100",
+        "OS_CPU":"45",
+        "OS_MEM":"65",
+        "OS_SWAP":"12",
+        "OS_STOR":"54",
+        "OS_IO":"110",}
+
+net_rec1={"NET_LINK":"100",
+         "NET_LOSS":"0",
+         "NET_LINKNUM":"382",
+         "NET_BW":"10",}
+
+phy_rec1={"PHY_FIRE":"100",
+         "PHY_WATER":"100",
+         "PHY_POW":"100",
+         "PHY_VOL":"100",
+         "PHY_TEM":"22",
+         "PHY_HUM":"43",}
+         
+
 def f(x,y):
     z = x + y
     z = np.array([[-2,-3,1,2,3,4],
@@ -16,6 +58,8 @@ def f(x,y):
     return z
  
 n = 1
+
+
  
 #均匀生成-3到3之间的n个值
 x = np.arange(-3,3,n)
