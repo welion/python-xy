24fcd27f3e0238e4a3988f8f02b1c04c12985430
welion <353566165@qq.com>
2016-05-12 22:55:29 +0800

    modify bs

--- bs4-learn.py
+++ bs4-learn.py
@@ -6,4 +6,21 @@ Created on Wed May 11 21:55:54 2016
 """
 
 from urllib.request import urlopen
-from 
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+    
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print "Title 
